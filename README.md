951a2d0717181803667cfb34b512fd2f9b81a6d1
Giovanni Merlino <gmerlino@unime.it>
2018-01-05 17:54:42 +0100

    Import

---
+++ README.md
@@ -0,0 +1,12 @@
+# Homebrew formula for udftools
+## How do I install this formula?
+`brew tap gmerlino/udftools` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/gmerlino/homebrew-udftools/master/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://github.com/Homebrew/brew/tree/master/share/doc/homebrew#readme).
---
+++ udftools.rb
@@ -0,0 +1,21 @@
+class Udftools < Formula
+  desc "Linux tools for UDF filesystems and DVD/CD-R(W) drives"
+  homepage "https://github.com/pali/udftools"
+
+  head "https://github.com/gmerlino/udftools.git", :branch => "brew"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
+
+  conflicts_with "udffsck", :because => "both install `udffsck` binary"
+
+  def install
+    if build.head?
+      inreplace "autogen.sh", "libtoolize", "glibtoolize"
+      system "sh", "autogen.sh"
+    end
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
